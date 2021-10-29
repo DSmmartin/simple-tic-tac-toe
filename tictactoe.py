cd42f7723b0c2f146958b4296f2cad1d04ec5132
Manuel José Martin Mairal <mjmm_manuel@hotmail.com>
2021-10-29 21:31:48 +0000

    add players to tictactoe human and ia

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,6 @@
 """Objects required to play TicTacToe game
 """
+import random
 import numpy as np
 
 
@@ -39,3 +40,20 @@ class TicTacToe:
             self.__show_board()
 
         self.__show_winner()
+
+
+class Player:
+    def make_a_move(self, position: int):
+        return position
+
+
+class HumanPlayer(Player):
+    def make_a_move(self):
+        position = int(input())  # TODO: make more robust
+        return super().make_a_move(position)
+
+
+class IAPlayer(Player):
+    def make_a_move(self):
+        position = round(random.uniform(0, 1))
+        return super().make_a_move(position)
