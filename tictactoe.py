082e78a2fa1a228f38cd79c946f91a8a90be62e5
Manuel José Martin Mairal <mjmm_manuel@hotmail.com>
2021-10-29 15:04:22 +0000

    add iterations track

--- tictactoe.py
+++ tictactoe.py
@@ -4,26 +4,29 @@ WINNER_STATUS = {1: 'Player 1', 2: 'Player 2', 3: 'draw', 0: None}
 
 
 class TicTacToe:
-
-    def __init__(self):
-        self.__board = np.empty((3, 3))
-        self.__winner_status = 0
+    _board = np.empty((3, 3))
+    _winner_status = 0
+    _iteration = 0
 
     def __show_board(self):
-        print(self.__board)  # TODO: improve visualization
+        print(f'-- Board at iteration: {self._iteration}')
+        print(self._board)  # TODO: improve visualization
 
     def __run_cycle(self):
-        self.__winner_status = 3
+        self._winner_status = 3
 
     def __show_winner(self):
-        if (self.__winner_status == 1) | (self.__winner_status == 2):
-            print(f'The winner is {WINNER_STATUS.get(self.__winner_status)}')
-        elif self.__winner_status == 3:
+        if (self._winner_status == 1) | (self._winner_status == 2):
+            print(f'The winner is {WINNER_STATUS.get(self._winner_status)}')
+        elif self._winner_status == 3:
             print('No winner in this game... :(')
 
     def start_game(self):
         print('starting game :) ')
-        while self.__winner_status == 0:
+        self.__show_board()
+        while self._winner_status == 0:
             self.__run_cycle()
+            self._iteration += 1
+            self.__show_board()
 
         self.__show_winner()
