236b6e77b8dbbeaf91c0a89df657ba6a4ad3a9e1
Manuel José Martin Mairal <mjmm_manuel@hotmail.com>
2021-11-01 20:38:46 +0000

    update board and add check filled method

--- tictactoe.py
+++ tictactoe.py
@@ -8,9 +8,13 @@ WINNER_STATUS = {1: 'Player 1', 2: 'Player 2', 3: 'draw', 0: None}
 
 
 class TicTacToe:
-    _board = np.empty((3, 3))
+    _length = 3
     _winner_status = 0
     _iteration = 0
+
+    def __init__(self) -> None:
+        self._board = np.empty((self._length, self._length), dtype='<U1')
+        self._board[:] = '-'
 
     def __str__(self) -> str:
         return """
@@ -23,13 +27,21 @@ class TicTacToe:
         print(self._board)  # TODO: improve visualization
 
     def __run_cycle(self):
-        self._winner_status = 3
+        self._winner_status = 1
 
     def __show_winner(self):
         if (self._winner_status == 1) | (self._winner_status == 2):
             print(f'The winner is {WINNER_STATUS.get(self._winner_status)}')
         elif self._winner_status == 3:
             print('No winner in this game... :(')
+
+    def __check_if_filled(self):
+        return '-' not in self._board
+
+    def __check_winner(self):
+        for line in self._length:
+            self._board[:, line]
+            self._board[line, :]
 
     def start_game(self):
         print('starting game :) ')
@@ -38,6 +50,8 @@ class TicTacToe:
             self.__run_cycle()
             self._iteration += 1
             self.__show_board()
+            if self.__check_if_filled():
+                self._winner_status = 3
 
         self.__show_winner()
 
