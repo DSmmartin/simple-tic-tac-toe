40b5add4bdc979e7a476346a13282ca3344a6615
Manuel José Martin Mairal <mjmm_manuel@hotmail.com>
2021-11-07 16:17:31 +0000

    update check winner

--- run_tictactoe.py
+++ run_tictactoe.py
@@ -2,4 +2,8 @@ from tictactoe import TicTacToe
 
 tictacttoe_game = TicTacToe()
 print(tictacttoe_game)
-tictacttoe_game.start_game()
+tictacttoe_game.show_board()
+for turn in tictacttoe_game:
+    tictacttoe_game.show_board()
+
+tictacttoe_game.show_winner()
--- tictactoe.py
+++ tictactoe.py
@@ -22,18 +22,33 @@ class TicTacToe:
         Instructions link: https://en.wikipedia.org/wiki/Tic-tac-toe
         """
 
-    def __show_board(self):
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._winner_status == 0:
+            self.__run_cycle()
+            self._iteration += 1
+            if self.__check_if_filled():
+                self._winner_status = 3
+        else:
+            raise StopIteration
+
+    def show_board(self):
         print(f'-- Board at iteration: {self._iteration}')
         print(self._board)  # TODO: improve visualization
 
     def __run_cycle(self):
         self.__check_winner()
 
-    def __show_winner(self):
+    def show_winner(self):
         if (self._winner_status == 1) | (self._winner_status == 2):
             print(f'The winner is {WINNER_STATUS.get(self._winner_status)}')
         elif self._winner_status == 3:
             print('No winner in this game... :(')
+
+        else:
+            print('Something went wrong...')
 
     def __check_if_filled(self):
         return '-' not in self._board
@@ -41,40 +56,31 @@ class TicTacToe:
     def __check_winner(self):
         """Check if in each row, column or diagonal there is a winner
         """
+        # check rows
         for i in range(self._length):
-            if self._board[i, 0] == self._board[i, 1] == self._board[i, 2]:
+            if ('-' not in self._board[i, :]) and (self._board[i, :] == self._board[i, 0]):
                 self._winner_status = self._board[i, 0]
                 return True
 
         # check columns
         for i in range(self._length):
-            if self._board[0, i] == self._board[1, i] == self._board[2, i]:
+            if ('-' not in self._board[:, i]) and (self._board[0, i] == self._board[0, i]):
                 self._winner_status = self._board[0, i]
                 return True
 
         # check diagonals
-        if self._board[0, 0] == self._board[1, 1] == self._board[2, 2]:
-            self._winner_status = self._board[0, 0]
+        board_diag = np.diag(self._board)
+        if ('-' not in board_diag) and (board_diag == board_diag[0]):
+            self._winner_status = board_diag[0]
             return True
 
-        if self._board[0, 2] == self._board[1, 1] == self._board[2, 0]:
-            self._winner_status = self._board[0, 2]
+        # obtain inverse diagonal of board
+        board_diag_inv = np.diag(np.fliplr(self._board))
+        if ('-' not in board_diag_inv) and (board_diag_inv == board_diag_inv[0]):
+            self._winner_status = board_diag_inv[0]
             return True
 
         return False
-
-    def start_game(self):
-        print('starting game :) ')
-        self.__show_board()
-        while self._winner_status == 0:
-            self.__run_cycle()
-            self._iteration += 1
-            self.__show_board()
-            if self.__check_if_filled():
-                self._winner_status = 3
-
-        self.__show_winner()
-
 
 class Player:
     def make_a_move(self, position: int):
